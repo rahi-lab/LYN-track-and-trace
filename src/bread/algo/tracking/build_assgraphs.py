b20347cd0f53c260c5d9aeda5a43ff4d49577dd4
Farzaneh <farzaneh.labbaf@epfl.ch>
2024-02-06 09:37:03 +0000

    Change inputs of build cellgraphs and assgraphs

--- src/bread/algo/tracking/build_assgraphs.py
+++ src/bread/algo/tracking/build_assgraphs.py
@@ -1,72 +1,84 @@
 if __name__ == '__main__':
-	from bread.data import Features, Segmentation
-	import bread.algo.tracking as tracking
-	from pathlib import Path
-	from glob import glob
-	from tqdm import tqdm
-	import pickle, json
-	import argparse
-	import os, sys, datetime
-	import torch
-	from importlib import import_module
+    from bread.data import Features, Segmentation
+    import bread.algo.tracking as tracking
+    from pathlib import Path
+    from glob import glob
+    from tqdm import tqdm
+    import pickle, json
+    import argparse
+    import os, sys, datetime
+    import torch
+    from importlib import import_module
 
-	
-	parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-	parser.add_argument('--config', dest='config', required=True, type=str, help='config file')
+    
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--config', dest='config', required=False, default='default', type=str, help='config file')
+    parser.add_argument('--cellgraphs_dir', dest='cellgraph_dir', required=False, default=None, type=str, help='filepaths to the cellgraph directories')
+    parser.add_argument('--output_folder', dest='output_folder',required=False, default=None, type=str, help='output directory')
+    parser.add_argument('--framediff_min', dest='framediff_min', required=False, default=None, type=int, help='minimum number of frame difference between two trackings')
+    parser.add_argument('--framediff_max', dest='framediff_max', required=False, default=None, type=int, help='maximum number of frame difference between two trackings')
+    parser.add_argument('--t1_max', dest='t1_max', type=int, required=False, default=None, help='maximum first frame')
+    parser.add_argument('--t1_min', dest='t1_min', type=int, required=False, default=None, help='minimum first frame')
 
-	# parser.add_argument('--cellgraphs', dest='cellgraph_dirs', required=True, type=str, help='filepaths to the cellgraph directories')
-	# parser.add_argument('--output_folder', dest='output_folder', required=True, type=Path, help='output directory')
-	# parser.add_argument('--framediff-min', dest='framediff_min', type=int, default=1, help='minimum number of frame difference between two trackings')
-	# parser.add_argument('--framediff-max', dest='framediff_max', type=int, default=12, help='maximum number of frame difference between two trackings')
-	# parser.add_argument('--t1-max', dest='t1_max', type=int, default=-1, help='maximum first frame')
+    args = parser.parse_args()
+    config = import_module("bread.config." + args.config).configuration.get('ass_graph_config')
+    if args.output_folder is not None:
+        config['output_folder'] = args.output_folder
+    if args.cellgraph_dir is not None:
+        config['cellgraph_dirs'] = [args.cellgraph_dir]
+    if args.framediff_min is not None:
+        config['framediff_min'] = args.framediff_min
+    if args.framediff_max is not None:
+        config['framediff_max'] = args.framediff_max
+    if args.t1_min is not None:
+        config['t1_min'] = args.t1_min
+    if args.t1_max is not None:
+        config['t1_max'] = args.t1_max
+    
+    print("build assignment graph with args: ", config)
 
-	args = parser.parse_args()
-	config = import_module("bread.config." + args.config).configuration.get('ass_graph_config')
-	print("build assignment graph with args: ", config)
+    os.makedirs(Path(config["output_folder"]), exist_ok=True)
+    extra = { 'node_attr': None, 'edge_attr': None, 'num_class_positive': 0, 'num_class_negative': 0 }
+    node_attr = []
+    edge_attr = []
 
-	os.makedirs(Path(config["output_folder"]), exist_ok=True)
-	extra = { 'node_attr': None, 'edge_attr': None, 'num_class_positive': 0, 'num_class_negative': 0 }
-	node_attr = []
-	edge_attr = []
+    for cellgraph_dir in tqdm(config["cellgraph_dirs"], desc='cellgraph'):
+        cellgraph_paths = list(sorted(glob(str(Path(cellgraph_dir) / 'cellgraph__*.pkl'))))
+        cellgraphs = []
+        for cellgraph_path in cellgraph_paths:
+            with open(cellgraph_path, 'rb') as file:
+                graph = pickle.load(file)
+            cellgraphs.append(graph)
 
-	
-	for cellgraph_dir in tqdm(config["cellgraph_dirs"], desc='cellgraph'):
-		cellgraph_paths = list(sorted(glob(str(Path(cellgraph_dir) / 'cellgraph__*.pkl'))))
-		cellgraphs = []
-		for cellgraph_path in cellgraph_paths:
-			with open(cellgraph_path, 'rb') as file:
-				graph = pickle.load(file)
-			cellgraphs.append(graph)
+        if config["t1_max"] == -1:
+            config["t1_max"] = len(cellgraphs)
+        # check if "t1_min" is in the config dictionary
+        if ("t1_min" in config):
+            t1_min = config["t1_min"]
+        else:
+            t1_min = 0
+            
+        name = Path(cellgraph_dir).stem
+        big_file_count = 0
+        for t1 in tqdm(range(max(0,t1_min),min(len(cellgraphs), config["t1_max"])), desc='t1', leave=False):
+            for t2 in tqdm(range(min(t1+config["framediff_min"], len(cellgraphs)), min(t1+config["framediff_max"]+1, len(cellgraphs))), desc='t2', leave=False):
+                nxgraph = tracking.build_assgraph(cellgraphs[t1], cellgraphs[t2], include_target_feature=True)
+                graph, node_attr, edge_attr = tracking.to_data(nxgraph, include_target_feature=True)
+                save_path = Path(config["output_folder"]) / f'{name}__assgraph__dt_{t2-t1:03d}__{t1:03d}_to_{t2:03d}.pt'
+                torch.save(graph, save_path)
 
-		if config["t1_max"] == -1:
-			config["t1_max"] = len(cellgraphs)
-		# check if "t1_min" is in the config dictionary
-		if ("t1_min" in config):
-			t1_min = config["t1_min"]
-		else:
-			t1_min = 0
-			
-		name = Path(cellgraph_dir).stem
-		big_file_count = 0
-		for t1 in tqdm(range(max(0,t1_min),min(len(cellgraphs), config["t1_max"])), desc='t1', leave=False):
-			for t2 in tqdm(range(min(t1+config["framediff_min"], len(cellgraphs)), min(t1+config["framediff_max"]+1, len(cellgraphs))), desc='t2', leave=False):
-				nxgraph = tracking.build_assgraph(cellgraphs[t1], cellgraphs[t2], include_target_feature=True)
-				graph, node_attr, edge_attr = tracking.to_data(nxgraph, include_target_feature=True)
-				save_path = Path(config["output_folder"]) / f'{name}__assgraph__dt_{t2-t1:03d}__{t1:03d}_to_{t2:03d}.pt'
-				torch.save(graph, save_path)
+                extra['num_class_positive'] += (graph.y == 1).sum()
+                extra['num_class_negative'] += (graph.y == 0).sum()
+                
 
-				extra['num_class_positive'] += (graph.y == 1).sum()
-				extra['num_class_negative'] += (graph.y == 0).sum()
-				
+    extra['node_attr'] = node_attr
+    extra['edge_attr'] = edge_attr
+    # convert a 0-dim tensor to a number
+    extra['num_class_positive'] = int(extra['num_class_positive'])
+    extra['num_class_negative'] = int(extra['num_class_negative'])
 
-	extra['node_attr'] = node_attr
-	extra['edge_attr'] = edge_attr
-	# convert a 0-dim tensor to a number
-	extra['num_class_positive'] = int(extra['num_class_positive'])
-	extra['num_class_negative'] = int(extra['num_class_negative'])
+    with open(Path(config["output_folder"]) / 'metadata.txt', 'w') as file:
+        file.write(f'Generated on {datetime.datetime.now()} with arguments {sys.argv}\n\n{config}')
 
-	with open(Path(config["output_folder"]) / 'metadata.txt', 'w') as file:
-		file.write(f'Generated on {datetime.datetime.now()} with arguments {sys.argv}\n\n{config}')
-
-	with open(Path(config["output_folder"]) / 'extra.json', 'w') as file:
-		json.dump(extra, file)
+    with open(Path(config["output_folder"]) / 'extra.json', 'w') as file:
+        json.dump(extra, file)
--- src/bread/algo/tracking/build_cellgraphs.py
+++ src/bread/algo/tracking/build_cellgraphs.py
@@ -12,22 +12,43 @@ if __name__ == '__main__':
 
 	
 	parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-	parser.add_argument('--config', dest='config', required=True, type=str, help='config file')
+	parser.add_argument('--config', dest='config', required=False, default='default', type=str, help='config file')
+	parser.add_argument('--input_segmentation', dest='segmentation', required=False, default=None, type=str, help='Address of a segmentation file')
+	parser.add_argument('--output_folder', dest= 'output_folder', required=False, default=None , type=str, help='Address of a folder to save cellgraphs')
+	parser.add_argument('--nn_threshold', dest = 'nn_threshold', default = None, type=int, help='threshold for nearest neighbor')
+	parser.add_argument('--scale_time', dest = 'scale_time', default = None, type=int, help='scale time by this number')
 	parser.add_argument('--fov', dest = 'fov', default = 'FOV0', type=str, help='if your segmentation file has multiple FOV, detrmine which one correlated with the microscopy files of the same name.')
+	parser.add_argument('--frame_max', dest = 'frame_max', default = -1, type=int, help='maximum number of frames to process')
+	parser.add_argument('--frame_min', dest = 'frame_min', default = 0, type=int, help='minimum number of frames to process')
 	
 	args = parser.parse_args()
 	config = import_module("bread.config." + args.config).configuration.get('cell_graph_config')
-	print("build cell graph args: ", config)
 
 	edge_features = config['edge_f_list']
 	cell_features = config['cell_f_list']
+	if args.output_folder is not None:
+		config['output_folder'] = args.output_folder
+	if args.nn_threshold is not None:
+		config['nn_threshold'] = args.nn_threshold
+	if args.scale_time is not None:
+		config['scale_time'] = args.scale_time
+	
+	
 	file_array = []
-	for filename in config['input_segmentations']:
+	if args.segmentation is not None:
+		config['input_segmentations'] = [args.segmentation]
+		input_segmentations = config['input_segmentations']
+	else:
+		input_segmentations = [args.segmentation]
+
+	print("build cell graph args: ", config)
+
+	for filename in input_segmentations:
 		if filename.endswith(".h5"):
 			file_array.append(filename)
-	config['input_segmentations'] = file_array
+	input_segmentations = file_array
 
-	for fp_segmentation in tqdm(config['input_segmentations'], desc='segmentation'):
+	for fp_segmentation in tqdm(input_segmentations, desc='segmentation'):
 		seg = SegmentationFile.from_h5(fp_segmentation).get_segmentation(args.fov)
 		try:
 			microscopy_file = Path(fp_segmentation).parent.parent /'microscopies'/ Path(fp_segmentation).stem
@@ -36,15 +57,22 @@ if __name__ == '__main__':
 			microscopy = Microscopy.from_tiff(microscopy_file)
 		except:
 			microscopy = None
-		feat = Features(seg, nn_threshold=config['nn_threshold'], scale_length=config['scale_length'], scale_time=config['scale_time'], microscopy=microscopy)
+		feat = Features(seg, nn_threshold=config['nn_threshold'], scale_time=config['scale_time'])
 		name = Path(fp_segmentation).stem
 		os.makedirs(Path(config["output_folder"]) / name, exist_ok=True)
 
-		for time_id in tqdm(range(len(seg)), desc='frame', leave=False):
+		if args.frame_max==-1:
+			frame_max = len(seg)
+		else:
+			frame_max = args.frame_max
+
+		for time_id in tqdm(range(max(0,args.frame_min), min(len(seg),frame_max)), desc='frame', leave=False):
 			graph = tracking.build_cellgraph(feat, time_id=time_id, 
 			cell_features=cell_features,
 			edge_features=edge_features
 			)
+			if(graph is None):
+				continue
 			with open(Path(config["output_folder"]) / name / f'cellgraph__{time_id:03d}.pkl', 'wb') as file:
 				pickle.dump(graph, file)
 
